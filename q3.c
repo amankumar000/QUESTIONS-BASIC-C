c057efa3e17d5a29913cbc6e5f9eb44104069254
amankumar000 <91332159+amankumar000@users.noreply.github.com>
2021-10-01 15:44:09 +0530

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int x,y;
+int sum;
+x=10;
+y=20;
+sum =x+y;
+printf("%d",sum);
+return 0;
+}
---
+++ q4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>/
+int main()
+{
+float x,y;
+float multiply;
+x =1.2;
+y =2.2;
+multiply =x*y;
+printf("%f", multiply);
+return 0;
+}
